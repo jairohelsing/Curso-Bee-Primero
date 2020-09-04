377f93070bd74579ae706a9c460eef8086484313
JairoPasuy <jairohelsing@hotmail.com>
2020-09-03 21:20:27 -0500

    prueba

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+h1 {
+    color: red;
+}
+</style>
+<body>
+    <p>holaa</p>
+    <p>chao</p>
+    <h1>hola</h1>
+</body>
+</html>
